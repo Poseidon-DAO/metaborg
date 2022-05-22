981ff2bc5201765671046f884b53aca5a5c80137
viganzeqiri <viganzeqirii@gmail.com>
2022-05-22 12:12:22 +0200

    made the button clickable to stop and play the video sound

--- components/home/Header.tsx
+++ components/home/Header.tsx
@@ -1,22 +1,46 @@
-import type { NextPage } from "next";
+import { useState } from "react";
 
 import { AudioButton, HamburgerMenu, Logo, SocialLinks } from "@metaborg";
 import { Navbar } from "@metaborg/Navbar";
 
-const Header: NextPage = () => {
-  return (
-    <header className="p-5 w-full fixed">
-      <div className="w-full lg:max-w-7xl mx-auto my-0 flex justify-between items-center">
-        <div className="flex justify-between items-center">
-          <AudioButton />
-          <Logo />
-          <Navbar />
-        </div>
+import type { NextPage } from "next";
+import type { Dispatch, SetStateAction } from "react";
 
-        <SocialLinks />
-        <HamburgerMenu />
-      </div>
-    </header>
+interface IHeaderProps {
+  renderHero?: ({
+    videoSound,
+    toggleVideoSound,
+  }: {
+    videoSound: boolean;
+    toggleVideoSound: Dispatch<SetStateAction<boolean>>;
+  }) => JSX.Element;
+}
+
+const Header: NextPage<IHeaderProps> = ({ renderHero }) => {
+  const [videoSound, setVideoSound] = useState(false);
+
+  const toggleVideoSound = () => setVideoSound((prevState) => !prevState);
+
+  return (
+    <>
+      <header className="p-5 w-full fixed z-10">
+        <div className="w-full lg:max-w-7xl mx-auto my-0 flex justify-between items-center">
+          <div className="flex justify-between items-center">
+            <AudioButton
+              videoSound={videoSound}
+              toggleVideoSound={toggleVideoSound}
+            />
+            <Logo />
+            <Navbar />
+          </div>
+
+          <SocialLinks />
+          <HamburgerMenu />
+        </div>
+      </header>
+
+      {renderHero?.({ videoSound, toggleVideoSound })}
+    </>
   );
 };
 
--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,14 +1,17 @@
 import type { NextPage } from "next";
 
-const Hero: NextPage = () => {
+interface IHeroProps {
+  videoSound: boolean;
+}
+
+const Hero: NextPage<IHeroProps> = ({ videoSound }) => {
   return (
     <section className="h-screen">
       <video
         src="https://metaborg.io/wp-content/uploads/2022/03/Teaser_website_2022_ok.mp4"
         autoPlay
         loop
-        muted
-        width="100%"
+        muted={!videoSound}
       ></video>
     </section>
   );
