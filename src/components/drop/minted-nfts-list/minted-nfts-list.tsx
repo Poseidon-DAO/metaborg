a4f8d05ce9b8edb85e9459c572d7a063d34bfd84
viganzeqiri <viganzeqirii@gmail.com>
2022-07-02 03:52:48 +0200

    added pdf reader

--- src/components/drop/minted-nfts-list/minted-nfts-list.tsx
+++ src/components/drop/minted-nfts-list/minted-nfts-list.tsx
@@ -1,14 +1,21 @@
 import {
   Box,
+  Container,
   Flex,
   Tab,
   TabList,
   TabPanel,
   TabPanels,
   Tabs,
+  Text,
 } from "@chakra-ui/react";
+import { Document, Page, pdfjs } from "react-pdf";
 import { Line } from "components/common";
 import { NextPage } from "next";
+
+import { tabs } from "./tabs";
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/legacy/build/pdf.worker.min.js`;
 
 interface IMintedNFTsListProps {
   nfts: any[];
@@ -23,52 +30,42 @@ const MintedNFTsList: NextPage<IMintedNFTsListProps> = () => {
 
       <Tabs isFitted variant="enclosed" borderColor="brand.transparent">
         <TabList mb="1em">
-          <Tab
-            _selected={{
-              color: "brand.red",
-              borderColor: "brand.red",
-              borderBottom: "none",
-            }}
-            borderBottomColor="brand.red"
-          >
-            Original
-          </Tab>
-          <Tab
-            _selected={{
-              color: "brand.red",
-              borderColor: "brand.red",
-              borderBottom: "none",
-            }}
-            borderBottomColor="brand.red"
-          >
-            Gold
-          </Tab>
-          <Tab
-            _selected={{
-              color: "brand.red",
-              borderColor: "brand.red",
-              borderBottom: "none",
-            }}
-            borderBottomColor="brand.red"
-          >
-            Diamond
-          </Tab>
+          {tabs.map((tab) => {
+            return (
+              <Tab
+                key={tab.tabId}
+                borderBottomColor="brand.red"
+                flexDir="column"
+                _selected={{
+                  color: "brand.red",
+                  borderColor: "brand.red",
+                  borderBottom: "none",
+                  fontWeight: "bold",
+                }}
+              >
+                <Text>Original</Text>
+                <Text fontSize="sm">You own: 3</Text>
+              </Tab>
+            );
+          })}
         </TabList>
 
         <TabPanels>
-          <TabPanel>
-            <p>original!</p>
-          </TabPanel>
-          <TabPanel>
-            <p>gold!</p>
-          </TabPanel>
-          <TabPanel>
-            <p>diamond!</p>
-          </TabPanel>
+          {tabs.map((tab) => {
+            return (
+              <TabPanel key={tab.tabId}>
+                <Container>
+                  <Document file="https://pinata.elysiumbridge.org/ipfs/QmbJb8dRPptsEr65uCyMcPACZtKbyMo2n167KUddcMYGbS">
+                    <Page pageNumber={1} />
+                  </Document>
+                </Container>
+              </TabPanel>
+            );
+          })}
         </TabPanels>
       </Tabs>
 
-      <Box mt={40}>
+      <Box mt={20}>
         <Line />
       </Box>
     </Flex>
---
+++ src/components/drop/minted-nfts-list/tabs.ts
@@ -0,0 +1,13 @@
+const tabs = [
+  {
+    tabId: 1,
+  },
+  {
+    tabId: 2,
+  },
+  {
+    tabId: 3,
+  },
+];
+
+export { tabs };
